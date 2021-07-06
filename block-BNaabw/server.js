b56c088ecfd9d630f753b10061cedc2f5410177b
Aditya Pathak <mynewchilly@gmail.com>
2021-07-06 15:37:18 +0530

    Re

--- block-BNaabw/server.js
+++ block-BNaabw/server.js
@@ -1,12 +1,3 @@
-// - add middleware for handling static assets
-// - add third party mddleware for logging and handling cookies
-// - add 2 sample routes with some random response of your choice
-
-//   1. GET -> `/` route
-//   2. GET -> `/users` route
-
-// - add middleware for handling errors
-
 const cookieParser = require("cookie-parser");
 let express = require("express");
 let logger = require("morgan");
@@ -15,9 +6,6 @@ let app = express();
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use((req, res) => {
-//   next("unauthorised");
-// });
 app.use(express.static("public"));
 app.use(logger());
 app.use(cookieParser());
@@ -33,20 +21,3 @@ app.get("/users", (req, res) => {
 app.listen(4000, () => {
   console.log("on server 4000...");
 });
-// Q. Using the above structure, convert a multipage HTML/CSS project into a node application.
-
-// - render all pages on seperate routes, for example
-//   - main page on `/` route
-//   - projects page on `/projects` routes etc..
-// - keep all static assets in public directory, for example
-
-//   - images in images/media directory
-//   - css in stylesheets directory
-
-//   Directory Structure should be
-
-//   - package.json
-//   - server.js // main server file
-//   - public // static asset path for images/css
-//   - index.html
-//   - projects.html and so on ..
